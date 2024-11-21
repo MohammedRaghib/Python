9b34ea73da50d13ad59dddc518c3a6841d3ea8a4
MohammedRaghib <mohammedmraghib@gmail.com>
2024-11-21 19:44:39 +0300

    made code more efficient

--- Extra.py
+++ Extra.py
@@ -9,3 +9,15 @@ def mockery():
         print(f'You typed this: "{sentence}"')
         mockery()
 mockery()
+
+
+def bettermock():
+    while True:
+        sentence = input('Type: ')
+
+        if sentence == 'exit':
+            print('Good day to you!')
+            break
+        else:
+            print(f'You said: {sentence}')
+bettermock()
